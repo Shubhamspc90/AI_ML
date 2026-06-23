71b48a18c06fbeaf4486e3f8542d892848b4956e
Shubham Chauhan <shubham.spc90@gmail.com>
2026-06-23 14:00:51 +0530

    constructor in python done

---
+++ python/oops/2_constructor.py
@@ -0,0 +1,48 @@
+"""In Python, a constructor is a special method that is automatically called when an object is created. 
+The constructor is defined using the __init__() method."""
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Creating an object
+p1 = Person("Shubham", 23)
+
+print(p1.name)  # Shubham
+print(p1.age)   # 23
+
+# Default Constructor
+class Student:
+    def __init__(self):
+        print("Student object created")
+
+s = Student()  # Student object created
+
+# Parameterized Constructor
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+c = Car("Toyota", "Camry")
+print(c.brand, c.model)  # Toyota Camry
+
+# Multiple Constructors?
+""" 
+Python does not support constructor overloading directly.
+You can achieve similar behavior using default arguments:
+"""
+
+class Employee:
+    def __init__(self, name, salary=0):
+        self.name = name
+        self.salary = salary
+
+e1 = Employee("John")
+e2 = Employee("John", 50000)
+
+print(e1.salary)  # 0
+print(e2.salary)  # 50000
+
+# So, in Python, the constructor is the __init__() method used to initialize an object when it is created.
