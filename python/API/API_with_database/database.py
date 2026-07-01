0513013b298a928dc4268cf9221766db5c5c79fd
Shubham Chauhan <shubham.spc90@gmail.com>
2026-07-01 11:05:00 +0530

    for database connection

---
+++ python/API/API_with_database/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "mysql+pymysql://root:root123@localhost/fastapi_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
