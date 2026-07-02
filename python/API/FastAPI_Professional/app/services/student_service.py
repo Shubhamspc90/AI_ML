44b133df4170e87d0df37679e4cb829fdc0396dc
Shubham Chauhan <shubham.spc90@gmail.com>
2026-07-02 13:25:05 +0530

    Introduce service layer for business logic

---
+++ python/API/FastAPI_Professional/app/services/student_service.py
@@ -0,0 +1,39 @@
+# ==========================================================
+# student_service.py
+#
+# Business Logic
+#
+# NOTE:
+# Yahan sirf Business Logic hoga.
+# Database query yahan nahi hogi.
+# ==========================================================
+
+from sqlalchemy.orm import Session
+
+from app import schemas
+from app.crud import student_crud
+
+# ==========================================================
+# CREATE STUDENT
+#
+# Business Logic
+# ==========================================================
+
+def create_student(
+    db: Session,
+    student: schemas.StudentCreate
+):
+
+    # Future Business Logic
+
+    # Example:
+    # Send Email
+    # Generate Roll Number
+    # Write Logs
+
+    # Call CRUD Layer
+
+    return student_crud.create_student(
+        db,
+        student
+    )
